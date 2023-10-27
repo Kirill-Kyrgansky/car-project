f90265e6f6e4eb27cf5d0664f0faa297b452b67a
kyrgansky.ku <kyrgansky.ku@zdmail.com>
2023-10-28 01:54:40 +0300

    добавлены стили во все блоки кроме header

--- src/templates/frame-border-container/frame-border-container.js
+++ src/templates/frame-border-container/frame-border-container.js
@@ -1,18 +1,20 @@
 export class MyFrame extends HTMLElement {
-    value = this.textContent
-    margin = this.getAttribute('margin')
-    connectedCallback() {
-        this.innerHTML =
-            `<div class="frame">
-                    <div class="frame-container">
-                        <p class="paragraph" 
+  value = this.textContent
+  margin = this.getAttribute('margin')
+  height = this.getAttribute('height')
+
+  connectedCallback() {
+    this.innerHTML =
+      `<div class='frame'>
+                    <div class='frame-container' style='height: ${this.height}'>
+                        <p class='paragraph' 
                         style='
                             margin: ${this.margin}'>
                             ${this.value}
                         </p>
                     </div>
             </div>`
-    }
+  }
 
 }
 
